36a087a65017fbf8f6fe72a2a850b9b630c0d385
MMASDEH <muhcis@gmail.com>
2023-10-27 07:40:18 +0400

    don't disptach sign in if isLoggedIn is true

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import CCarousel from "../components/carousel";
 import CCards from "../components/cards";
 import { getCookie } from "../services/token.service";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { singIn } from "../store/login.slice";
 export const Home = () => {
-  
+
   const dispatch = useDispatch();
   const cookie = getCookie();
-  if (cookie) {
-    dispatch(singIn())
+  const isLoggedIn = useSelector(state => state.login.isLoggedIn)
+  if (!isLoggedIn) {
+    if (cookie) {
+      dispatch(singIn())
+    }
   }
   return (
     <>
